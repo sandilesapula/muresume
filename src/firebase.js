5f5522afca37fc0eccf5449f61fed983b0ffdf6e
davidsandilesapula <davidsandilesapula@gmail.com>
2021-03-19 17:09:55 +0200

    resume

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDGjWR8OlC28PPRF57RT195RgARoj1vZ0A",
+//     authDomain: "my-resume-a415d.firebaseapp.com",
+//     projectId: "my-resume-a415d",
+//     storageBucket: "my-resume-a415d.appspot.com",
+//     messagingSenderId: "229486610746",
+//     appId: "1:229486610746:web:a2d7cf5ac8206805626803",
+//     measurementId: "G-1HK543XPC1"
+//   };
